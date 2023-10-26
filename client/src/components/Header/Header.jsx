b43ac6c027bb2bfe20d640b35ac1eadb8c1ba21d
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-26 15:50:20 +0600

    Implemented Some Custom CSS

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<header className="header">
+			<Container>
+				<Row>
+               <div className="nav__wrapper"></div>
+            </Row>
+			</Container>
+		</header>
+	);
 };
 
 export default Header;
